b111d2e2f3c2561fa321348e545eabb1acae9c58
nickname <81326152@qq.com>
2025-02-07 23:42:32 +0800

    rm input putput

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -127,12 +127,16 @@ export class Pair {
     }
     const inputReserve = this.reserveOf(inputAmount.token)
     const outputReserve = this.reserveOf(inputAmount.token.equals(this.token0) ? this.token1 : this.token0)
-    const inputAmountWithFee = JSBI.multiply(inputAmount.raw, _997)
-    const numerator = JSBI.multiply(inputAmountWithFee, outputReserve.raw)
-    const denominator = JSBI.add(JSBI.multiply(inputReserve.raw, _1000), inputAmountWithFee)
+    // const inputAmountWithFee = JSBI.multiply(inputAmount.raw, _997)
+    // const numerator = JSBI.multiply(inputAmountWithFee, outputReserve.raw)
+    // const denominator = JSBI.add(JSBI.multiply(inputReserve.raw, _1000), inputAmountWithFee)
+    // const outputAmount = new TokenAmount(
+    //   inputAmount.token.equals(this.token0) ? this.token1 : this.token0,
+    //   JSBI.divide(numerator, denominator)
+    // )
     const outputAmount = new TokenAmount(
       inputAmount.token.equals(this.token0) ? this.token1 : this.token0,
-      JSBI.divide(numerator, denominator)
+      outputReserve.raw
     )
     if (JSBI.equal(outputAmount.raw, ZERO)) {
       throw new InsufficientInputAmountError()
@@ -152,11 +156,15 @@ export class Pair {
 
     const outputReserve = this.reserveOf(outputAmount.token)
     const inputReserve = this.reserveOf(outputAmount.token.equals(this.token0) ? this.token1 : this.token0)
-    const numerator = JSBI.multiply(JSBI.multiply(inputReserve.raw, outputAmount.raw), _1000)
-    const denominator = JSBI.multiply(JSBI.subtract(outputReserve.raw, outputAmount.raw), _997)
+    // const numerator = JSBI.multiply(JSBI.multiply(inputReserve.raw, outputAmount.raw), _1000)
+    // const denominator = JSBI.multiply(JSBI.subtract(outputReserve.raw, outputAmount.raw), _997)
+    // const inputAmount = new TokenAmount(
+    //   outputAmount.token.equals(this.token0) ? this.token1 : this.token0,
+    //   JSBI.add(JSBI.divide(numerator, denominator), ONE)
+    // )
     const inputAmount = new TokenAmount(
       outputAmount.token.equals(this.token0) ? this.token1 : this.token0,
-      JSBI.add(JSBI.divide(numerator, denominator), ONE)
+      inputReserve.raw
     )
     return [inputAmount, new Pair(inputReserve.add(inputAmount), outputReserve.subtract(outputAmount))]
   }
